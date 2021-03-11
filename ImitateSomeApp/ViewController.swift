c2eed0593807c34bb28b4333878fe26e5c034c69
Parker Chen <parkerlived@gmail.com>
2021-03-11 15:50:29 +0800

    Initial Commit

---
+++ ImitateSomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImitateSomeApp
+//
+//  Created by Parker Chen on 2021/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
